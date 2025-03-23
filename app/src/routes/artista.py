44a39a18973e178a323fd0b4b6a09fca112fb43c
M-a-t-e-o <874162@unizar.es>
2025-03-23 20:53:30 +0100

    change-datos-artista + swagger album
    Co-authored-by: Rachel <rachel-cd-04@users.noreply.github.com>

--- app/src/routes/artista.py
+++ app/src/routes/artista.py
@@ -3,8 +3,19 @@ from db.models import Oyente, Artista, Album
 from db.db import get_db
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask import Blueprint, request, jsonify
+import cloudinary.uploader
+import os
 
 artista_bp = Blueprint('info', __name__) 
+
+cloudinary.config(
+  cloud_name = os.getenv('CLOUDINARY_NAME'),
+  api_key = os.getenv('CLOUDINARY_KEY'),
+  api_secret = os.getenv('CLOUDINARY_SECRET'),
+  secure = True
+)
+
+
 
 """Devuelve informacion de un artista"""
 @artista_bp.route("/get-mis-datos-artista", methods=["GET"])
@@ -26,6 +37,7 @@ def get_mis_datos_artista():
             "numSeguidores": len(artista.seguidores),
             "biografia" : artista.biografia
             }), 200
+
 
 """Devuelve una lista con los álbumes de un artista"""
 @artista_bp.route('/get-mis-albumes', methods=['GET'])
@@ -50,3 +62,52 @@ def get_mis_albumes():
         ]
     
     return jsonify({"albumes": albumes}), 200
+
+
+"""Actualiza los datos de un artista"""
+@artista_bp.route('/change-datos-artista', methods=['PUT'])
+@jwt_required()
+@tokenVersion_required()
+@roles_required("artista")
+def change_datos_artista():
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Datos incorrectos."}), 400
+
+    correo = get_jwt_identity()
+    foto_perfil = data.get('fotoPerfil')
+    nombre_usuario = data.get('nombreUsuario')
+    nombre_artistico = data.get('nombreArtistico')
+
+    if not foto_perfil and not nombre_usuario and not nombre_artistico:
+        return jsonify({"error": "Faltan datos para actualizar el artista."}), 400
+
+    with get_db() as db:
+        artista_entry = db.get(Artista, correo)
+        if not artista_entry:
+            return jsonify({"error": "El artista no existe."}), 404
+
+        if foto_perfil:
+            foto_antigua = artista_entry.fotoPerfil
+            public_id = foto_antigua.split('/')[-1].split('.')[0]
+
+            try:
+                cloudinary.uploader.destroy(public_id, resource_type="image")
+            except Exception as e:
+                return jsonify({"error": f"Error al eliminar la foto de perfil antigua de Cloudinary: {e}"}), 500
+
+            artista_entry.fotoPerfil = foto_perfil
+
+        if nombre_usuario:
+            # Verificar si el nombre de usuario ya existe
+            existing_user = db.query(Oyente).filter_by(nombreUsuario=nombre_usuario).first()
+            if existing_user:
+                return jsonify({"error": "El nombre de usuario ya está en uso."}), 400
+            artista_entry.nombreUsuario = nombre_usuario
+
+        if nombre_artistico:
+            artista_entry.nombreArtistico = nombre_artistico
+
+        db.commit()
+
+    return jsonify({"message": "Datos del artista actualizados exitosamente."}), 200
