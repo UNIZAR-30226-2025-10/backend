efc52e9acd0b495c3b369c5b68b4f58c37343a7c
baruben <873680@unizar.es>
2025-03-26 16:02:57 +0100

    subir archivos a cloudinary

--- app/src/routes/cloudinary.py
+++ app/src/routes/cloudinary.py
@@ -1,10 +1,11 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
 from db.models import *
 from utils.decorators import roles_required, tokenVersion_required
 import os
 import cloudinary
+import time
 
 cloudinary_bp = Blueprint('cloudinary', __name__) 
 
@@ -15,15 +16,33 @@ cloudinary.config(
   secure = True
 )
 
-"""Devuelve las credenciales de Cloudinary"""
-@cloudinary_bp.route('/get-cloudinary-credentials', methods=['GET'])
+"""Devuelve la firma para hacer peticiones a cloudinary"""
+@cloudinary_bp.route('/get-signature', methods=['GET'])
 @jwt_required()
 @tokenVersion_required()
-@roles_required("artista")
-def get_cloudinary_credentials():
-    credentials = {
-        "cloud_name": cloudinary.config().cloud_name,
-        "api_key": cloudinary.config().api_key,
-        "api_secret": cloudinary.config().api_secret,
-    }
-    return jsonify(credentials), 200
+@roles_required("oyente","artista")
+def generate_signed_upload():
+    folder = request.args.get("folder")
+    if not folder:
+        return jsonify({"error": "Faltan datos en la peticion."}), 400
+    try:
+        # Crear parámetros para firmar la solicitud
+        params_to_sign = {
+            "timestamp": int(time.time()),  # Usar el módulo time correctamente
+            "folder": folder  # Establecer un folder para las cargas
+        }
+
+        # Generar la firma usando la API de Cloudinary
+        signature = cloudinary.utils.api_sign_request(params_to_sign, cloudinary.config().api_secret)
+
+        # Devolver la respuesta en formato JSON
+        return jsonify({
+            "signature": signature,
+            "api_key": cloudinary.config().api_key,
+            "timestamp": params_to_sign["timestamp"],
+            "cloud_name": cloudinary.config().cloud_name
+        }), 200
+
+    except Exception as e:
+        # En caso de error, devolver un mensaje de error
+        return jsonify({"error": str(e)}), 500
