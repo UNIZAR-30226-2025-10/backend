b30f67fd192face51cd152649a9cd2af49fe38ce
marta-gl0 <879128@unizar.es>
2025-03-27 13:41:44 +0100

    get-estadisticas-playlists + swagger + cambios minimos en estadisticas

--- app/src/utils/estadisticas.py
+++ app/src/utils/estadisticas.py
@@ -1,11 +1,20 @@
 from db.models import EsParteDePlaylist, Cancion, Playlist
-from sqlalchemy import select, exists, and_
+from sqlalchemy import select
 
 def estadisticas_song(cancion_entry, db):    
-    n_playlists = db.query(EsParteDePlaylist).filter_by(Cancion_id=cancion_entry.id).count()
-            
-    favs = db.query(EsParteDePlaylist).join(Playlist).filter(
-        EsParteDePlaylist.Cancion_id == cancion_entry.id, Playlist.nombre == "Favoritos"
-    ).count()
+    stmt_n_playlists = select(EsParteDePlaylist.Cancion_id).where(
+        EsParteDePlaylist.Cancion_id == cancion_entry.id
+    )
+    n_playlists = len(db.scalars(stmt_n_playlists).all())
+
+    stmt_favs = (
+        select(EsParteDePlaylist.Cancion_id)
+        .join(Playlist, EsParteDePlaylist.Playlist_id == Playlist.id)
+        .where(
+            EsParteDePlaylist.Cancion_id == cancion_entry.id,
+            Playlist.nombre == "Favoritos"
+        )
+    )
+    favs = len(db.scalars(stmt_favs).all())
     
     return n_playlists, favs
