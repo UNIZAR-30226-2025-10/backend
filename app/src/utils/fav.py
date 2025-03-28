557cfa0c9fd9b7297168ff8894db7ecf0fa84e6d
baruben <873680@unizar.es>
2025-03-28 10:32:54 +0100

    arreglado bug fav en canciones sueltas

--- app/src/utils/fav.py
+++ app/src/utils/fav.py
@@ -3,10 +3,11 @@ from sqlalchemy import select, exists, and_
 
 "Devuelve si una cancion esta o no en la lista de Favoritos de un usuario"
 def fav(id, correo, db):
-    stmt = select(exists().where(and_(
-        EsParteDePlaylist.Cancion_id == id,
-        EsParteDePlaylist.Playlist_id == Playlist.id,
-        Playlist.nombre == "Favoritos",
-        Playlist.Oyente_correo == correo
-    )))
-    return db.scalar(stmt)
+    stmt_fav = select(EsParteDePlaylist.Cancion_id).join(Playlist
+        ).where(and_(
+            Playlist.nombre == "Favoritos",
+            Playlist.Oyente_correo == correo, 
+            EsParteDePlaylist.Cancion_id == id
+        )
+    )
+    return db.execute(stmt_fav).scalar() is not None
