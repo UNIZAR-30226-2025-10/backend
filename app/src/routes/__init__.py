09d122f2a33beeba1897be0aeba6e30a9a8203de
baruben <873680@unizar.es>
2025-04-02 11:41:11 +0200

    solucion bug al quitar files

--- app/src/routes/__init__.py
+++ app/src/routes/__init__.py
@@ -3,7 +3,6 @@ from .admin import admin_bp
 from .auth import auth_bp 
 from .home import home_bp
 from .search import search_bp
-from .files import files_bp
 from .artista import artista_bp
 from .song import song_bp
 from .playlist import playlist_bp
@@ -18,7 +17,6 @@ def register_routes(app):
     app.register_blueprint(admin_bp)
     app.register_blueprint(home_bp)
     app.register_blueprint(search_bp)
-    app.register_blueprint(files_bp)
     app.register_blueprint(artista_bp)
     app.register_blueprint(song_bp)
     app.register_blueprint(playlist_bp)
