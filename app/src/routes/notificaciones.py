4e52e8dd676305103700fa967ac28a8fafb75244
marta-gl0 <879128@unizar.es>
2025-04-07 19:20:59 +0200

    notificaciones parte 2

--- app/src/routes/notificaciones.py
+++ app/src/routes/notificaciones.py
@@ -113,3 +113,139 @@ def get_novedades_musicales():
         resultado.extend(format_album(a) for a in albumes[j:])
 
         return jsonify({"resultado": resultado}), 200
+
+
+"""Marca todas notificaciones de interacciones con un noizzy del usuario logueado como leidas"""
+@notificacion_bp.route("/read-interacciones", methods=["PATCH"])
+@jwt_required()
+@tokenVersion_required()
+@roles_required("artista", "oyente")
+def read_interacciones():
+    correo = get_jwt_identity()
+    with get_db() as db:
+        NoizzyAlias = aliased(Noizzy)
+        stmt = select(Noizzito
+                ).join(NoizzyAlias, Noizzito.Noizzy_id == NoizzyAlias.id
+                ).where(and_(NoizzyAlias.Oyente_correo == correo,
+                    Noizzito.visto == False))
+        
+        respuestas = db.execute(stmt).scalars().all()
+        
+        stmt = select(Like
+                ).join(Noizzy, Noizzy.id == Like.Noizzy_id
+                ).where(and_(Noizzy.Oyente_correo == correo,
+                    Like.visto == False))
+        
+        likes = db.execute(stmt).scalars().all()
+        
+        if not likes and not respuestas:
+            return jsonify({"error": "No existen notificaciones de interacciones."}), 404
+
+        for respuesta in respuestas:
+            respuesta.visto = True
+
+        for like in likes:
+            like.visto = True
+
+        try:
+            db.commit() 
+        except Exception as e:
+            db.rollback()
+            return jsonify({"error": "Ha ocurrido un error inesperado.", "details": str(e)}), 500
+    
+    return jsonify(""), 200
+
+
+"""Elimina todas las notificaciones de novedades musicales del usuario logueado"""
+@notificacion_bp.route("/delete-novedades-musicales", methods=["DELETE"])
+@jwt_required()
+@tokenVersion_required()
+@roles_required("artista", "oyente")
+def delete_novedades_musicales():    
+    correo = get_jwt_identity()
+    with get_db() as db:
+        stmt_canciones = delete(notificacionCancion_table).where(notificacionCancion_table.c.Oyente_correo == correo)
+        stmt_albumes = delete(notificacionAlbum_table).where(notificacionAlbum_table.c.Oyente_correo == correo)
+
+        result_canciones = db.execute(stmt_canciones)
+        result_albumes = db.execute(stmt_albumes)
+        if result_canciones.rowcount == 0 and result_albumes.rowcount == 0:
+            return jsonify({"error": "No existen notificaciones de novedades musicales."}), 404
+        
+        try:
+            db.commit() 
+        except Exception as e:
+            db.rollback()
+            return jsonify({"error": "Ha ocurrido un error inesperado.", "details": str(e)}), 500
+    
+    return jsonify(""), 204
+
+
+"""Devuelve una lista con las notificaciones de noizzito"""
+@notificacion_bp.route("/get-interacciones", methods=["GET"])
+@jwt_required()
+@tokenVersion_required()
+@roles_required("artista", "oyente")
+def get_interacciones():
+    correo = get_jwt_identity()
+    with get_db() as db:
+        NoizzyAlias = aliased(Noizzy)
+        stmt = select(Noizzito
+                ).join(NoizzyAlias, Noizzito.Noizzy_id == NoizzyAlias.id
+                ).where(and_(NoizzyAlias.Oyente_correo == correo,
+                    Noizzito.visto == False)
+                ).options(
+                    selectinload(Noizzito.oyente)
+                ).order_by(Noizzito.fecha.desc())
+        
+        respuestas = db.execute(stmt).scalars().all()
+        
+        stmt = select(Like
+                ).join(Noizzy, Noizzy.id == Like.Noizzy_id
+                ).where(and_(Noizzy.Oyente_correo == correo,
+                    Like.visto == False)
+                ).options(
+                    selectinload(Like.oyente),
+                    selectinload(Like.noizzy)    
+                ).order_by(Like.fecha.desc())
+        
+        likes = db.execute(stmt).scalars().all()
+        
+        resultado = []
+        i, j = 0, 0
+        while i < len(respuestas) and j < len(likes):
+            if respuestas[i].fecha > likes[j].fecha:
+                resultado.append({
+                    "nombreUsuario": respuestas[i].oyente.nombreUsuario,
+                    "noizzy": respuestas[i].id,
+                    "texto": respuestas[i].texto,
+                    "tipo": "respuesta"
+                })
+                i += 1
+            else:
+                resultado.append({
+                    "nombreUsuario": likes[j].oyente.nombreUsuario,
+                    "noizzy": likes[j].noizzy.id,
+                    "texto": likes[j].noizzy.texto,
+                    "tipo": "like"
+                })
+                j += 1
+
+        for r in respuestas[i:]:
+            resultado.append({
+                    "nombreUsuario": r.oyente.nombreUsuario,
+                    "noizzy": r.noizzy.id,
+                    "texto": r.noizzy.texto,
+                    "tipo": "respuesta"
+                })
+
+        for l in likes[j:]:
+            resultado.append({
+                    "nombreUsuario": l.oyente.nombreUsuario,
+                    "noizzy": l.id,
+                    "texto": l.texto,
+                    "tipo": "like"
+                })
+
+        return jsonify(resultado), 200
+
